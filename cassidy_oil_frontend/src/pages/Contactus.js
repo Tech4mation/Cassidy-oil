86aad6c179141c7ec884b55f80ed99a9aa8e2bfe
Jtobyy <jtobi8161@gmail.com>
2023-05-03 23:32:56 +0100

    Update pictures

--- cassidy_oil_frontend/src/pages/Contactus.js
+++ cassidy_oil_frontend/src/pages/Contactus.js
@@ -6,32 +6,34 @@ export default function ContactUs() {
                 }}>
                 <h1 className="fw-bold display-3" style={{color: '#777777'}}>Contact Us</h1>
             </div>
-            <div className="p-5">
-            <form>
-                <div className="row">
-                    <div className="col">
-                        <label for="firstName">First Name</label>
-                        <input type="text" id="firstName" className="mt-2 form-control form-control-lg" />
+            <div className="p-5 mb-5">
+                <form>
+                    <div className="row">
+                        <div className="col">
+                            <label for="firstName">First Name</label>
+                            <input type="text" id="firstName" className="mt-2 form-control form-control-lg" />
+                        </div>
+                        <div className="col">
+                            <label for="firstName">Last Name</label>    
+                            <input type="text" id="lastName" className="mt-2 form-control form-control-lg" />
+                        </div>
                     </div>
-                    <div className="col">
-                        <label for="firstName">Last Name</label>    
-                        <input type="text" id="lastName" className="mt-2 form-control form-control-lg" />
+                    <div className="row mt-4">
+                        <div className="col">
+                            <label for="firstName">Phone Number</label>
+                            <input type="phone" id="firstName" className="mt-2 form-control form-control-lg" />
+                        </div>
+                        <div className="col">
+                            <label for="firstName">Email Address</label>    
+                            <input type="email" id="lastName" className="mt-2 form-control form-control-lg" />
+                        </div>
                     </div>
-                </div>
-                <div className="row mt-4">
-                    <div className="col">
-                        <label for="firstName">Phone Number</label>
-                        <input type="phone" id="firstName" className="mt-2 form-control form-control-lg" />
+                    <div className="form-group mt-5">
+                        <textarea placeholder="type message" class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
                     </div>
-                    <div className="col">
-                        <label for="firstName">Email Address</label>    
-                        <input type="email" id="lastName" className="mt-2 form-control form-control-lg" />
-                    </div>
-                </div>
-                <div className="form-group mt-5">
-                    <textarea placeholder="type message" class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
-                </div>
-            </form>            
+
+                    <button type="submit" className="mt-4 py-md-3 btn btn-warning text-white w-50" style={{backgroundColor: '#F8A135'}}>Send</button>
+                </form>            
             </div>                
         </>
     )
